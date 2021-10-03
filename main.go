ca33d5319bee16d322d04240eec46dfdef5abbd2
Pankaj <singh.pankaj88848@gmail.com>
2021-10-03 18:50:43 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"covidapi/app/api"
+	"covidapi/app/handlers"
+	"covidapi/mongodb"
+	"time"
+
+	_ "covidapi/docs"
+
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+func main() {
+
+	e := echo.New()
+	go func() {
+		for {
+			final_data := api.GettingData()
+			// fmt.Println(final_data)
+			mongodb.InsertingNewData(final_data)
+			time.Sleep(30 * time.Minute)
+		}
+	}()
+	e.GET("GetStateData", handlers.GetCases)
+	e.GET("/GetAllData", handlers.GetAllCases)
+	e.GET("/GetByGeoLocation", handlers.GetDataFromGeoLocation)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+	e.Logger.Fatal(e.Start(":8080"))
+}
